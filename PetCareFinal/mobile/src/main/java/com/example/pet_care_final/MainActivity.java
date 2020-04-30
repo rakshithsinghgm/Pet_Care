34b744eeac63ab0f83051ece440b4c8d0ebae89f
Tom <clunietp@gmail.com>
2020-04-29 20:40:18 -0400

    Watch and UI fixes

--- PetCareFinal/mobile/src/main/java/com/example/pet_care_final/MainActivity.java
+++ PetCareFinal/mobile/src/main/java/com/example/pet_care_final/MainActivity.java
@@ -1,51 +1,17 @@
 package com.example.pet_care_final;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
-import android.widget.TextView;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.wearable.DataEvent;
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.DataItem;
-import com.google.android.gms.wearable.DataMapItem;
-import com.google.android.gms.wearable.MessageClient;
-import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.PutDataMapRequest;
-import com.google.android.gms.wearable.PutDataRequest;
-import com.google.android.gms.wearable.Wearable;
-
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -60,11 +26,6 @@ public class MainActivity extends AppCompatActivity  {
 
     private SQLiteDatabase statsdb;
     private Cursor cur;
-
-    public static final String ACTIVITY_CLASS_SLEEPING_STRING = "Sleeping";
-    public static final String ACTIVITY_CLASS_INACTIVE_STRING = "Inactive";
-    public static final String ACTIVITY_CLASS_ACTIVE_STRING = "Active";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
